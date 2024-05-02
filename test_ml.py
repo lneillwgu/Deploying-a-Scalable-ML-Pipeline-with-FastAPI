af72bae462a95399e8ada4bdc9448add40a0131f
lneillwgu <lneill4@wgu.com>
2024-05-02 02:42:36 +0000

    Edits to test_ml

--- test_ml.py
+++ test_ml.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-from ml.model import (train_model,compute_model_metrics)
+from ml.model import (train_model,compute_model_metrics, load_model, inference)
 from ml.data import (apply_label,process_data)
 # TODO: add necessary import
 
@@ -56,7 +56,15 @@ def test_three(data):
     """
     # Your code here
     train, test = train_test_split(data, test_size= 0.25, random_state=42)
+    
     assert train.shape[0]>750,('Training Data has greater than 750 entries')
     assert test.shape[0]>250,('Testing Data has greater than 250 entries')
+
+
+
+
+
+
+
 
  
