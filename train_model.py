16bb40c40aa2c4329bf49005b2a7ba933f130eb5
lneillwgu <lneill4@wgu.com>
2024-05-02 00:11:36 +0000

    Edited train_model syntax

--- train_model.py
+++ train_model.py
@@ -36,14 +36,14 @@ cat_features = [
 
 #TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
-    train,
+    X=train,
     categorical_features=cat_features,
     label="salary",
     training=True,
     )
 
 X_test, y_test, _, _ = process_data(
-    test,
+    X=test,
     categorical_features=cat_features,
     label="salary",
     training=False,
@@ -94,4 +94,3 @@ for col in cat_features:
         with open("slice_output.txt", "a") as f:
             print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
             print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
-#indented 89 and 90
