87ff2eaf14607dcae0385cc5167a7a8f476bedff
lneillwgu <lneill4@wgu.com>
2024-05-03 01:11:38 +0000

    Added welcome message and filled in TODO sections

--- main.py
+++ main.py
@@ -26,21 +26,23 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = # TODO: enter the path for the saved encoder 
+project_path = os.getcwd()
+path = os.path.join(project_path, "model", "encoder.pkl")
 encoder = load_model(path)
 
-path = # TODO: enter the path for the saved model 
+path = os.path.join(project_path, "model", "model.pkl")
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
+    return {"Hello!": "Welcome to the API"}
+    
 
 
 # TODO: create a POST on a different path that does model inference
@@ -65,10 +67,10 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+        X=data,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder
     )
-    _inference = # your code here to predict the result using data_processed
+    _inference = inference(model,data_processed)
     return {"result": apply_label(_inference)}
