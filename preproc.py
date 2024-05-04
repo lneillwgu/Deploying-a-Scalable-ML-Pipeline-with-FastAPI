6f115ea255ce06ac8f46ad9edf6ae64c536d08a2
lneillwgu <lneill4@wgu.com>
2024-05-04 00:10:40 +0000

    Minor changes to preprocessing file

--- preproc.py
+++ preproc.py
@@ -1,15 +1,14 @@
-#Created this to preprocess the data to remove duplicates discovered
-#during data exploration as well as any whitespaces
+# Created this to preprocess the data to remove duplicates discovered
+# during data exploration as well as any whitespaces
 
 import pandas as pd
-import numpy
 import os
 
 project_path = os.getcwd()
 data_path = os.path.join(project_path, "data", "census.csv")
 df= pd.read_csv(data_path)
 
-df.drop_duplicates(inplace=True)
+df.drop_duplicates(inplace = True)
 
 df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
 
