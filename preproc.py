c4dda290deb397c5d30b69e238c90479c3626fc2
lneillwgu <lneill4@wgu.com>
2024-05-04 00:13:02 +0000

    Minor changes to preproc.py

--- preproc.py
+++ preproc.py
@@ -6,9 +6,9 @@ import os
 
 project_path = os.getcwd()
 data_path = os.path.join(project_path, "data", "census.csv")
-df= pd.read_csv(data_path)
+df = pd.read_csv(data_path)
 
-df.drop_duplicates(inplace = True)
+df.drop_duplicates(inplace=True)
 
 df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
 
